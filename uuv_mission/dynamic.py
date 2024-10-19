deb1773499f128709f0e394773d9eef99e23380c
LeiliSab <leili.sabourian@seh.ox.ac.uk>
2024-10-19 13:01:25 +0000

    Calling controller in Closed Loop Class

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -103,6 +103,8 @@ class ClosedLoop:
             positions[t] = self.plant.get_position()
             observation_t = self.plant.get_depth()
             # Call your controller here
+            reference_t = mission.reference[t]
+            actions[t] = self.controller.compute_control(reference_t, observation_t)
             self.plant.transition(actions[t], disturbances[t])
 
         return Trajectory(positions)
